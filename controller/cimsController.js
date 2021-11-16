bd1e860836597c0cf2a06add13ad03906f65f58b
radhikam-vb <89972314+radhikam-vb@users.noreply.github.com>
2021-11-16 18:29:12 +0530

    Update cimsController.js

--- controller/cimsController.js
+++ controller/cimsController.js
@@ -1,22 +1,6 @@
-import { Comp } from "../model/compSchema.js"
-
-
-export const cimsGet = (req, res)=>{
-    res.send("Get record in cims")
-}
-
-export const cimsPost = async(req, res)=>{
-    const {CompanyUID, CompanyName, PrimaryContact, SecondaryContact, TertiaryContact} = req.body
-
-    try {
-        const newComp = await Comp.create({CompanyUID, CompanyName, PrimaryContact, SecondaryContact, TertiaryContact})
-        res.json(newComp)
-    } catch (err) {
-        console.log(err)
-    }
-=======
+mport { Comp } from "../model/compSchema.js"
 export const cimsGet = async(req, res)=>{
-    const {CompanyUID, CompanyName, PrimaryContact} = req.body
+    const {Designation, BrandName, ClientName, Domain, BaseLocation, CompanyAddress, Contacts }=req.body
 
     try {
         const Comps = await Comp.find();
@@ -25,11 +9,17 @@ export const cimsGet = async(req, res)=>{
         res.status(500).send(error)
     }
 }
-export const cimsPost = (req, res)=>{
-    res.send("Post record in cims")
 
+export const cimsPost = async(req, res)=>{
+    const {Designation, BrandName, ClientName, Domain, BaseLocation, CompanyAddress, Contacts}=req.body
+
+    try {
+        const newComp = await Comp.create({Designation, BrandName, ClientName, Domain, BaseLocation, CompanyAddress, Contacts })
+        res.json(newComp)
+    } catch (err) {
+        console.log(err)
+    }
 }
-
 
 export const cimsDel = (req, res)=>{
     res.send("Delete record in cims")
@@ -38,5 +28,3 @@ export const cimsDel = (req, res)=>{
 export const cimsPatch = (req, res)=>{
     res.send("Patch record in cims")
 }
-
-
