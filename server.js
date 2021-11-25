4dadc4b9a8e496437671a650fb0fb02f7e37a121
Kunal Bisht <kunal.b@valuebound.com>
2021-11-25 12:21:42 +0530

    edit in returning data structure

--- server.js
+++ server.js
@@ -35,10 +35,33 @@ app.post('/login', (req, res)=>{
 app.get('/location', async (req, res)=>{
     const pincode = req.headers.pincode
     const country = req.headers.country
+    
     const url = `https://api.worldpostallocations.com/pincode?postalcode=${pincode}&countrycode=${country}`
     const fetch_res = await fetch(url)
-    const data = await fetch_res.json()
-    res.json(data) 
+    
+    const location = await fetch_res.json()
+    const state = location.result[0].state
+    //const postalLocations = location.result.map(dists => {
+            //if(dists.district == "East Delhi")
+            //{
+              //  console.log(dists.postalLocation)
+                //return dists.postalLocation
+            //}
+        //})
+
+        const districts = location.result.reduce(function(result, current) {
+            result[current.district] = result[current.district] || [];
+            result[current.district].push(current.postalLocation);
+            return result;
+        }, {})
+    
+
+    const data = new Object({
+        state,
+        districts
+    })
+    
+    res.json(data)
 })
 
 app.get('/countries', async (req, res)=>{
